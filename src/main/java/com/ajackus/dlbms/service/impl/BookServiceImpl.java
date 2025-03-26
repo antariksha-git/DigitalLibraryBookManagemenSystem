5815902c772788164748e8bbc8415b52a7af136a
antariksha-sharma <helloantariksha@gmail.com>
2025-03-27 00:54:11 +0530

    Add mapper to reduce redudantant code

--- src/main/java/com/ajackus/dlbms/service/impl/BookServiceImpl.java
+++ src/main/java/com/ajackus/dlbms/service/impl/BookServiceImpl.java
@@ -2,14 +2,13 @@ package com.ajackus.dlbms.service.impl;
 
 import com.ajackus.dlbms.dto.BookRequestDto;
 import com.ajackus.dlbms.dto.BookResponseDto;
-import com.ajackus.dlbms.entity.Status;
+import com.ajackus.dlbms.mapper.BookMapper;
 import com.ajackus.dlbms.model.Book;
 import com.ajackus.dlbms.repository.BookRepository;
 import com.ajackus.dlbms.service.BookService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -17,28 +16,13 @@ import java.util.UUID;
 public class BookServiceImpl implements BookService {
 
     private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
     @Override
     public BookResponseDto createBook(BookRequestDto bookRequestDto) {
-        Book book = new Book();
-        book.setBookId(bookRequestDto.getBookId());
-        book.setTitle(bookRequestDto.getTitle());
-        book.setAuthor(bookRequestDto.getAuthor());
-        book.setGenre(bookRequestDto.getGenre());
-        book.setAvailabilityStatus(Status.valueOf(bookRequestDto.getAvailabilityStatus()));
+        Book book = bookMapper.toEntity(bookRequestDto);
         book.setUuid(UUID.randomUUID().toString().substring(0, 8));
 
-        Optional<Book> savedBook = Optional.of(bookRepository.save(book));
-
-        return savedBook.map(myBook -> {
-            BookResponseDto bookResponseDto = new BookResponseDto();
-            bookResponseDto.setBookId(myBook.getBookId());
-            bookResponseDto.setUuid(myBook.getUuid());
-            bookResponseDto.setTitle(myBook.getTitle());
-            bookResponseDto.setAuthor(myBook.getAuthor());
-            bookResponseDto.setGenre(myBook.getGenre());
-            bookResponseDto.setAvailabilityStatus(myBook.getAvailabilityStatus());
-            return bookResponseDto;
-        }).orElse(null);
+        return bookMapper.toDto(bookRepository.save(book));
     }
 }
